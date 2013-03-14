7610046578d45a0a7f7645b6a78c42327a925662
Dylan Savage <dylan.savage@gmail.com>
2013-03-14 15:45:23 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taldb::Application.config.secret_token = 'd3aac66aa8636bf86b53d1639b183f4f2135fa22e3cf78ad728595118b9ba6b730e9c2cfcec1afbe3fb930c5b4ae96a9d471976368490b831f347f308e8df3d4'
